59c2906067e58d0e9d638a72dc4f2de5881d8e90
Anas <anaska1311@gmail.com>
2026-04-08 21:23:34 +0800

    Initial Commit

---
+++ Spendr/SpendrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendrApp.swift
+//  Spendr
+//
+//  Created by Anas Azman on 08/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpendrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
